24d870f3337a5f2d3fe73bc8789b4c31e5bde112
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-12 18:33:57 +0530

    used case 1

---
+++ EmployeeWage_UC1_Present_Absent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_UC1_Present_Absent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int empPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+                Console.WriteLine("Random Value:" + empCheck);
+            if (empPresent == empCheck)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+                Console.ReadLine();
+            }
+        }
+    }
