7261962aa4962eeb30e61a772e3446bba39b31cd
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-26 18:14:31 +0530

    Employee Wage For Multiple Companies

--- EmployeeWage_UC1_Present_Absent/Program.cs
+++ EmployeeWage_UC1_Present_Absent/Program.cs
@@ -6,46 +6,61 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage_UC1_Present_Absent
 {
-    internal class Program
+    public class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
+        /// <summary>
+        /// Defines Entry Point of Application
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //Variables
-            int empHrs = 0, totalEmphrs = 0, totalWorkingDays = 0;
-            //Computation
-            while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            //UC-8 Compute Employee Wage for Multiple Companies
+            CalculateEmpWage("Google", 20, 30, 120);
+            CalculateEmpWage("Microsoft", 30, 40, 150);
+            CalculateEmpWage("Infosys", 20, 40, 120);
+            Console.ReadLine();
+        }
+
+        public static void CalculateEmpWage(string COMPANY, int EMP_RATE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS)
+        {
+            //we are not changing values of variable EMP_PRESENT and EMP_RATE_HR thats why we decalred it as constatnt
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            //const int EMP_RATE_PER_HR = 20;
+            //const int MAX_WORKING_DAYS = 20;
+            //const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalEmpWage = 0, day = 1, totalHrs = 0;
+            Random random = new Random();//Generate Random number
+
+            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
-                totalWorkingDays++;
-                Random random = new Random();
                 int empCheck = random.Next(0, 3);
+                //Console.WriteLine("Random Value: " + empCheck);
+
                 switch (empCheck)
                 {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
+                    case FULL_TIME:
                         empHrs = 8;
+                        //Console.WriteLine("Full Time Employee is Present");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("Part Time Employee is Present");
                         break;
                     default:
                         empHrs = 0;
+                        //Console.WriteLine("Employee is Absent");
                         break;
                 }
-                totalEmphrs += empHrs;
-                Console.WriteLine("Day#" + totalWorkingDays + " Emp Hrs  : " + empHrs);
-                //Console.ReadLine();
-
+                empWage = empHrs * EMP_RATE_PER_HR;
+                //Console.WriteLine("Comapany Name: {0}",COMPANY);
+                //Console.WriteLine("Employee Wage per on day{0} and Hrs:{1} is: {2} ", day, totalHrs, empWage);
+                totalEmpWage += empWage;
+                day++;
+                totalHrs += empHrs;
             }
-            int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("total Emp Wage : " + totalEmpWage);
-            Console.ReadLine();
-
-
+            Console.WriteLine("\nComapany Name: {0}", COMPANY);
+            Console.WriteLine("Employee Wage for Month is: " + totalEmpWage);
         }
     }
 }
-
